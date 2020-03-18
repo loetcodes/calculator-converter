8d2d56eaebbb0859eebb739b2263799ee567b7cf
lulets-l <46016065+lulets@users.noreply.github.com>
2020-03-18 22:13:51 +0400

    added sliding effect for calculator to converter change

--- js/main.js
+++ js/main.js
@@ -1,20 +1,56 @@
-// Calculator and Currency Converter.
-const currencies = "../currency.json";
-const exchangeApi = "https://api.exchangerate-api.com/v4/latest/";
+// Main JS for Currency Converter and Calculator.
 
-const convertCurrency = (currFrom, currTo) => {
-  // Returns the conversion ratio for a currFrom to a currTo
-  let inputCurr = currFrom;
-  let outputCurr = currTo;
-  let currencyRate;
-  // If exchange rate date is current, use current json.
+// Displays the calculator
+const displayCalculator = document.getElementById("calculator-btn");
+displayCalculator.addEventListener("click", () => {
+  addClass("feature-content", "slide-right");
+});
 
-  // Fetch the exchange rate if exchange rate is old
-  fetch(exchangeApi + currFrom)
-    .then(result => result.json())
-    .then(result => {
-      currencyRate = result.rates[currTo];
-      console.log('Currency rate is', currencyRate);
-    })
+// Displays the converter
+const displayConverter = document.getElementById("converter-btn");
+displayConverter.addEventListener("click", () => {
+  removeClass("feature-content", "slide-right");
+});
 
+const addClass = (elementId, classItem) => {
+  // Adds a given class to an element.
+  let element = document.getElementById(elementId);
+  if (!element.classList.contains(classItem)) {
+    element.classList.add(classItem);
+  }
 }
+
+const removeClass = (elementId, classItem) => {
+  // Removes a given class from an element.
+  let element = document.getElementById(elementId);
+  if (element.classList.contains(classItem)) {
+    element.classList.remove(classItem);
+  }
+}
+
+
+
+
+
+
+
+
+// const currencies = "../currency.json";
+// const exchangeApi = "https://api.exchangerate-api.com/v4/latest/";
+
+// const convertCurrency = (currFrom, currTo) => {
+//   // Returns the conversion ratio for a currFrom to a currTo
+//   let inputCurr = currFrom;
+//   let outputCurr = currTo;
+//   let currencyRate;
+//   // If exchange rate date is current, use current json.
+
+//   // Fetch the exchange rate if exchange rate is old
+//   fetch(exchangeApi + currFrom)
+//     .then(result => result.json())
+//     .then(result => {
+//       currencyRate = result.rates[currTo];
+//       console.log('Currency rate is', currencyRate);
+//     })
+
+// }

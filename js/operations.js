35c0a234afbd388f5741240baf1320cdc45bbac2
lulets-l <46016065+lulets@users.noreply.github.com>
2020-04-02 23:06:38 +0400

    change to operations functions

--- js/operations.js
+++ js/operations.js
@@ -3,37 +3,37 @@
 export class Operations {
   add = (a, b) => {
     // Adds two numbers.
-    let inputValidity = (typeof(a) !== 'number' || typeof(b) !== 'number');
+    let inputValidity = (Number.isNaN(a) || Number.isNaN(b));
     return inputValidity ? false : a + b;
   }
 
   subtract = (a, b) => {
     // Subtracts two numbers.
-    let inputValidity = (typeof(a) !== 'number' || typeof(b) !== 'number');
+    let inputValidity = (Number.isNaN(a) || Number.isNaN(b));
     return inputValidity ? false : a - b;
   }
 
   multiply = (a, b) => {
     // Multiplies two numbers.
-    let inputValidity = (typeof(a) !== 'number' || typeof(b) !== 'number');
+    let inputValidity = (Number.isNaN(a) || Number.isNaN(b));
     return inputValidity ? false : a * b;
   }
 
   divide = (a, b) => {
     // Divides two numbers.
-    let inputValidity = (typeof(a) !== 'number' || typeof(b) !== 'number');
+    let inputValidity = (Number.isNaN(a) || Number.isNaN(b));
     return inputValidity || (b === 0 || b === 0.0) ? false : a / b;
   }
 
   modulus = (a, b) => {
     // Modulus of two numbers.
-    let inputValidity = (typeof(a) !== 'number' || typeof(b) !== 'number');
+    let inputValidity = (Number.isNaN(a) || Number.isNaN(b));
     return inputValidity || (b === 0 || b === 0.0) ? false : a % b;
   }
 
   plusMinus = (a) => {
     // Negates a value.
-    return typeof(a) !== 'number' ? false : -1 * a;
+    return Number.isNaN(a) ? false : -1 * a;
   }
 }
 
